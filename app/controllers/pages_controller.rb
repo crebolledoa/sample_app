47d41a1907e08b65b472cecc78dc1bbb3e61d32d
Consuelo Rebolledo <consuelo.rebolledo@inzpiral.com>
2012-11-12 11:03:50 -0300

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
   def home
+  	@title = "Home" # @ means it's an instance variable
   end
 
   def contact
+  	@title = "Contact"
+  end
+
+  def about
+  	@title = "About"
   end
 end
